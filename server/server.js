7c1091a6e28802d69fd08f550cbbe734ed723760
dogarx1-arch <fankarx1@gmail.com>
2026-02-25 00:41:33 +0500

    Updated CORS for production

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 // Change this to your frontend URL later when you deploy
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173'];
 
 app.use(express.json());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
